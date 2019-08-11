9f1f1ead029991b31f98ee8a2cede7b989951a08
Pedro Pessoa <pedro.pessoa@futurice.com>
2019-08-11 11:52:38 +0300

    Make users and tasks endpoints

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+require("./db/mongoose");
+
+const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,8 +10,29 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.post("/users", (request, response) => {
-  console.log(request.body);
-  response.send("sending post");
+  const user = new User(request.body);
+
+  user
+    .save()
+    .then(() => {
+      response.send(user);
+    })
+    .catch(error => {
+      response.status(400).send(error);
+    });
+});
+
+app.post("/tasks", (request, response) => {
+  const task = new Task(request.body);
+
+  task
+    .save()
+    .then(() => {
+      response.send(task);
+    })
+    .catch(error => {
+      response.status(400).send(error);
+    });
 });
 
 app.listen(port, () => {
