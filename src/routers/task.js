f81cd2d57dd2b414040e59f4d348b5270feaa34c
Pedro Pessoa <pedro.pessoa@futurice.com>
2019-08-15 18:44:58 +0300

    Sort query by asc and desc order

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,11 +20,18 @@ router.post("/tasks", auth, async (request, response) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=3&skip=0
+// GET /tasks?sortBy=createdAt // updatedAt
 router.get("/tasks", auth, async (request, response) => {
   const match = {};
+  const sort = {};
 
   if (request.query.completed) {
     match.completed = request.query.completed === "true";
+  }
+
+  if (request.query.sortBy) {
+    const parts = request.query.sortBy.split(":");
+    sort[parts(0)] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
@@ -34,7 +41,8 @@ router.get("/tasks", auth, async (request, response) => {
         match,
         options: {
           limit: parseInt(request.query.limit),
-          skip: parseInt(request.query.skip)
+          skip: parseInt(request.query.skip),
+          sort
         }
       })
       .execPopulate();
